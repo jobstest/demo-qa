e3f5c2fe601d39e3b1e081d07b7922e63de9c7a1
Vladimir Parfionov <jobstest18@gmail.com>
2022-09-01 12:42:28 +0400

    added test for TestBox

--- src/test/java/guru/qa/TestBoxTests.java
+++ src/test/java/guru/qa/TestBoxTests.java
@@ -1,13 +1,43 @@
 package guru.qa;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestBoxTests {
 
+    @BeforeAll
+    static void setUp() {
+        //Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
     @Test
     void fillFormTest() {
-        open("https://demoqa.com/text-box");
+        String name = "Тестов Тест";
+        String email = "test@mail.ru";
+        String currentAddress = "University of Cambridge, The Old Schools, Trinity Lane, Cambridge CB2 1TN, UK";
+        String permanentAddress = "University of Oxford University Offices Wellington Square Oxford OX1 2JD United Kingdom";
+
+        open("/text-box");
+        $("[id=userName]").setValue(name);
+        $("[id=userEmail]").setValue(email);
+        $("[id=currentAddress]").setValue(currentAddress);
+        $("[id=permanentAddress]").setValue(permanentAddress);
+        $("[id=submit]").click();
+        /*$("[id=output]").shouldHave(text(name),
+                text(email),
+                text(currentAddress),
+                text(permanentAddress));*/
+        $("[id=output] [id=name]").shouldHave(text(name));
+        $("[id=output]").$("[id=email]").shouldHave(text(email));
+        $("p[id=currentAddress]").shouldHave(text(currentAddress));
+        $("[id=permanentAddress]", 1).shouldHave(text(permanentAddress));
     }
 }
